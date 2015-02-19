a6c0441fb1054c05dd1ebe03ead5800475edbc82
John Halley <johnhalley@Johns-MacBook-Air.local>
2015-02-18 20:32:12 -0800

    Initial Commit

---
+++ Canvas Assignment #2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Canvas Assignment #2
+//
+//  Created by John Halley on 2/18/15.
+//  Copyright (c) 2015 John Halley. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
